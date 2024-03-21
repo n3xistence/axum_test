8203fe6ef206411f11ab6dfd836c1e8a392c3170
n3xistence <maxinies00@gmail.com>
2024-03-21 23:05:26 +0100

    feat(router): add cors rules to restrict external access

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
 mod handlers;
 mod models;
 
-use handlers::{default, users};
+use handlers::{database, default, users};
 
+use axum::http::{HeaderValue, Method};
 use axum::{routing::get, Router};
 use dotenv::dotenv;
+use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
+
+    let cors_layer = CorsLayer::new()
+        .allow_methods([Method::GET, Method::POST, Method::DELETE])
+        .allow_origin("http://localhost:8080".parse::<HeaderValue>().unwrap());
 
     let address = dotenv::var("ADDRESS").unwrap();
     let port = dotenv::var("PORT").unwrap();
@@ -16,9 +22,12 @@ async fn main() {
     let binding = format!("{}:{}", address, port);
 
     let app = Router::new()
-        .route("/", get(default::status))
+        .route("/", get(default::not_implemented))
+        .route("/status", get(default::status))
         .route("/user", get(users::get_all_users))
-        .route("/user/:id", get(users::get_user_by_id));
+        .route("/user/:id", get(users::get_user_by_id))
+        .route("/users", get(database::get_all_users))
+        .layer(cors_layer);
 
     let listener = tokio::net::TcpListener::bind(binding).await.unwrap();
     axum::serve(listener, app).await.unwrap();
