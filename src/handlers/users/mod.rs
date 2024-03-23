bf73f092db72ef55dab49a4731731dfb16214a4b
n3xistence <maxinies00@gmail.com>
2024-03-23 19:51:45 +0100

    feat(user): make user id optional to allow passing of elements to insert
    Due to auto increment, we need to pass the user without an id

--- src/handlers/users/mod.rs
+++ src/handlers/users/mod.rs
@@ -4,18 +4,21 @@ use serde_json::{json, Value};
 use crate::models::entities::User;
 
 pub async fn get_all_users() -> Json<Value> {
-    let user = User::new(String::from("Alice"), 1);
+    let user = User::new(String::from("Alice"), Some(1), String::from("email"));
 
     Json(json!(user))
 }
 
 pub async fn get_user_by_id(Path(id): Path<u32>) -> Result<Json<Value>, StatusCode> {
     let users = vec![
-        User::new(String::from("Alice"), 1),
-        User::new(String::from("Bob"), 2),
+        User::new(String::from("Alice"), Some(1), String::from("email")),
+        User::new(String::from("Bob"), Some(2), String::from("email")),
     ];
 
-    match users.into_iter().find(|user| user.id == id) {
+    match users.into_iter().find(|user| match user.id {
+        Some(user_id) => user_id == id,
+        None => false,
+    }) {
         Some(user) => Ok(Json(json!(user))),
         None => Err(StatusCode::NOT_FOUND),
     }
